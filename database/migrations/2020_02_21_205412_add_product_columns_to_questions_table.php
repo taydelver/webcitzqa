8837ad21c4d676c2c7328420b21d1fbdb3067874
Taylor Delver <taylor@webcitz.com>
2020-02-21 14:56:29 -0600

    added questions column to migrations

---
+++ database/migrations/2020_02_21_205412_add_product_columns_to_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProductColumnsToQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->string('product_name')->nullable();
+            $table->string('product_sku')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+}
